f022b21289eea16fd38e36ba9deae7e714f12d77
grkndev <gweepcreative@gmail.com>
2025-09-15 19:30:42 +0300

    refactor: change timer input from minutes to seconds

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -10,7 +10,7 @@ export default function HomeScreen() {
       <SelectPreview />
 
       <PomodoroTimer
-        initialMinutes={0.1}
+        initialSeconds={5}
         onComplete={() => {
           console.log("Pomodoro session completed from HomeScreen!");
         }}
