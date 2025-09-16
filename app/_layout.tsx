75eae81cb3c5dccf0072b9dfad29731cea780164
grkndev <gweepcreative@gmail.com>
2025-09-16 12:44:18 +0300

    Pomodoro Sheet

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,25 @@
 import "react-native-reanimated";
 
-
 import "@/global.css";
 import { NAV_THEME } from "@/lib/theme";
-import { Montserrat_400Regular, Montserrat_500Medium, Montserrat_600SemiBold, Montserrat_700Bold, Montserrat_800ExtraBold, Montserrat_900Black, useFonts } from "@expo-google-fonts/montserrat";
 import {
-  ThemeProvider
-} from "@react-navigation/native";
+  Montserrat_400Regular,
+  Montserrat_500Medium,
+  Montserrat_600SemiBold,
+  Montserrat_700Bold,
+  Montserrat_800ExtraBold,
+  Montserrat_900Black,
+  useFonts,
+} from "@expo-google-fonts/montserrat";
+import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+import { ThemeProvider } from "@react-navigation/native";
 import { PortalHost } from "@rn-primitives/portal";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 
 import * as SplashScreen from "expo-splash-screen";
 import React from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 export const unstable_settings = {
   anchor: "(tabs)",
 };
@@ -37,14 +44,16 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={NAV_THEME["light"]}>
-      
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        
-      </Stack>
-      <PortalHost />
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <BottomSheetModalProvider>
+        <ThemeProvider value={NAV_THEME["light"]}>
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          </Stack>
+          <PortalHost />
+          <StatusBar style="auto" />
+        </ThemeProvider>
+      </BottomSheetModalProvider>
+    </GestureHandlerRootView>
   );
 }
